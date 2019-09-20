61f3c2d64e8dfbf892ac9c9b2de335225251c68f
Kelwin Arthur Pereira Vitor <kelwin@nupad.medicina.ufmg.br>
2019-09-20 10:47:31 -0300

    Updates

---
+++ precache-manifest.8e50931c38828d9abedff09d33c37508.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897ad9fb87fc4c0a2effa9072afa771d",
+    "url": "/Unicafe/index.html"
+  },
+  {
+    "revision": "a00279e0a7a579152674",
+    "url": "/Unicafe/static/js/2.9f12ddfc.chunk.js"
+  },
+  {
+    "revision": "191eb1fcc60b20a4556c",
+    "url": "/Unicafe/static/js/main.48e231d7.chunk.js"
+  },
+  {
+    "revision": "a6d5111e27c2fbc3c729",
+    "url": "/Unicafe/static/js/runtime~main.e1cc6682.js"
+  }
+]);
